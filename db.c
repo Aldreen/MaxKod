30fc9846a8201f7610ad5aad9c73e5fff51c1204
Aldreen <Maxih@live.se>
2014-10-02 16:41:11 +0200

    added actions.h and base help file

--- db.c
+++ db.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <init.h>
+#include <actions.h>
 
 typedef struct node{
   char *key;
@@ -30,7 +31,7 @@ int getOpt(){
 	return choice;
 }
 
-void queryKey()
+void queryKey(){
 // Query
       printf("Enter key: ");
       readline(buffer, 128, stdin);
@@ -49,118 +50,9 @@ void queryKey()
       if(!found){
         printf("Could not find an entry matching key \"%s\"!\n", buffer);
       }
+	}
 
 
-void doSmt(int option)
-	switch(option)
-	 case 1:
-     queryKey();
-	 
-      break;
-    case 2:
-      // Update
-      printf("Enter key: ");
-      readline(buffer, 128, stdin);
-      puts("Searching database...\n");
-      found = 0;
-      cursor = list;
-      while(!found && cursor != NULL){
-        if(strcmp(buffer, cursor->key) == 0){
-          puts("Matching entry found:");
-          printf("key: %s\nvalue: %s\n\n", cursor->key, cursor->value);
-          found = 1;
-        }else{
-          cursor = cursor->next;
-        }
-      }
-      if(!found){
-        printf("Could not find an entry matching key \"%s\"!\n", buffer);
-      }else{
-        printf("Enter new value: ");
-        readline(buffer, 128, stdin);
-        free(cursor->value);
-        cursor->value = malloc(strlen(buffer) + 1);
-        strcpy(cursor->value, buffer);
-        puts("Value inserted successfully!");
-      }
-      break;
-    case 3:
-      // Insert
-      printf("Enter key: ");
-      readline(buffer, 128, stdin);
-      puts("Searching database for duplicate keys...");
-      found = 0;
-      cursor = list;
-      while(!found && cursor != NULL){
-        if(strcmp(buffer, cursor->key) == 0){
-          printf("key \"%s\" already exists!\n", cursor->key);
-          found = 1;
-        }else{
-          cursor = cursor->next;
-        }
-      }
-      if(!found){ // Insert new node to the front of the list
-        puts("Key is unique!\n");
-        Node newNode = malloc(sizeof(struct node));
-        newNode->key = malloc(strlen(buffer) + 1);
-        strcpy(newNode->key, buffer);
-        printf("Enter value: ");
-        readline(buffer, 128, stdin);
-        newNode->value = malloc(strlen(buffer) + 1);
-        strcpy(newNode->value, buffer);
-        newNode->next = list;
-        list = newNode;
-        puts("");
-        puts("Entry inserted successfully:");
-        printf("key: %s\nvalue: %s\n", list->key, list->value);
-      }
-      break;
-    case 4:
-      // Delete
-      printf("Enter key: ");
-      readline(buffer, 128, stdin);
-      puts("Searching database...\n");
-      found = 0;
-      cursor = list;
-      Node prev = NULL;
-      while(!found && cursor != NULL){
-        if(strcmp(buffer, cursor->key) == 0){
-          if(prev == NULL){ // Delete first node
-            list = cursor->next;
-          }else{
-            prev->next = cursor->next;
-          }
-          found = 1;
-          printf("Deleted the following entry:\nkey: %s\nvalue: %s\n", cursor->key, cursor->value);
-        }else{
-          prev = cursor;
-          cursor = cursor->next;
-        }
-      }
-      if(!found){
-        printf("Could not find an entry matching key \"%s\"!\n", buffer);
-      }
-      break;
-    case 5:
-      // Print database
-      cursor = list;
-      while(cursor != NULL){
-        puts(cursor->key);
-        puts(cursor->value);
-        cursor = cursor->next;
-      }
-      break;
-    case 0:
-      // Exit
-      puts("Good bye!");
-      break;
-    default:
-      // Please try again
-      puts("Could not parse choice! Please try again");
-    }
-    puts("");
-  }
-  return 0;
 
 
 int main(int argc, char *argv[]){
